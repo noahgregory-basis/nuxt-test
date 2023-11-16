a33b70afaaf3fae861adef12c5f10437dffd9526
Noah Gregory <noah.gregory@basisworldwide.com>
2023-11-16 11:54:16 -0500

    add components

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
+<script setup lang="ts">
+import Bar from './components/Bar.vue';
+</script>
 <template>
   <div>
-    <NuxtWelcome />
+    <Bar />
   </div>
 </template>
---
+++ components/Bar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Foo from './Foo.vue';
+const foos = ref(null);
+console.log('foo!');
+const bar = 0;
+</script>
+<template>
+  <Foo v-for="_ in 5" ref="foos" />
+</template>
+<style>
+div {
+  margin: v-bind(bar);
+}
+</style>
